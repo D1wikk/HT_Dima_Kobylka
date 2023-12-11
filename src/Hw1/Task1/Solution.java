e0b00b0def3a8b840c750449ffb01938e2c29935
D1wikk <vdmk04@gmail.com>
2023-12-11 20:06:39 +0200

    first commit

--- src/Hw1/Task1/Solution.java
+++
@@ -1,8 +0,0 @@
-package Hw1.Task1;
-
-public class Solution {
-    public static void main(String[] args) {
-        // виводимо на екран заданий текст
-        System.out.println("Студент дуже розумний");
-    }
-}
